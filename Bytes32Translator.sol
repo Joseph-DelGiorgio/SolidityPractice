320ced4880f5c682524515d3cb9f36ce3b71f13c
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-09-21 10:12:13 -0700

    Create Bytes32Translator.sol

---
+++ Bytes32Translator.sol
@@ -0,0 +1,30 @@
+//This contract has functions that allow user to convert a string to bytes32, as well as bytes32 to a string.
+
+
+pragma solidity 0.8.7;
+//SPDX-License-Identifier: MIT
+
+contract MyBytes{
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+}
+}
