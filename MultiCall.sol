49f969e4e7869ffef976e8d530d013160aa72034
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-11-15 20:33:10 -0800

    Create MultiCall.sol

---
+++ MultiCall.sol
@@ -0,0 +1,35 @@
+Tutorial by Smart Contract Programmer Youtube Channel
+video link: https://youtu.be/PDR054Cy8qM
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract MultiCall {
+    function multiCall(address[] calldata targets, bytes[] calldata data)
+        external
+        view
+        returns (bytes[] memory)
+    {
+        require(targets.length == data.length, "target length != data length");
+
+        bytes[] memory results = new bytes[](data.length);
+
+        for (uint i; i < targets.length; i++) {
+            (bool success, bytes memory result) = targets[i].staticcall(data[i]);
+            require(success, "call failed");
+            results[i] = result;
+        }
+
+        return results;
+    }
+}
+
+contract TestMultiCall {
+    function test(uint _i) external pure returns (uint) {
+        return _i;
+    }
+
+    function getData(uint _i) external pure returns (bytes memory) {
+        return abi.encodeWithSelector(this.test.selector, _i);
+    }
+}
