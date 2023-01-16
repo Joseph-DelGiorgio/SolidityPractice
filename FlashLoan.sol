a4c44d003058588db7248e4eb6198f530f6337d7
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-16 11:26:22 -0800

    Update FlashLoan.sol

--- FlashLoan.sol
+++ FlashLoan.sol
@@ -77,3 +77,30 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
 
     receive() external payable {}
 }
+
+
+
+
+
+//Deploy.js
+
+
+const hre = require("hardhat");
+
+async function main() {
+  console.log("deploying...");
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy(
+    "0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D"
+  );
+
+  await flashLoan.deployed();
+
+  console.log("Flash loan contract deployed: ", flashLoan.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
