b87cc072c378531178791aa67721d9c15fa2a86b
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-10-18 14:22:33 -1000

    Update FreeLance.sol

--- FreeLance.sol
+++ FreeLance.sol
@@ -1,1 +1,60 @@
+pragma solidity ^0.8.0;
 
+contract DecentralizedFreelancePlatform {
+    struct Job {
+        address client;
+        string description;
+        uint budget;
+        uint deadline;
+        address selectedFreelancer;
+        bool jobCompleted;
+        bool clientApproved;
+    }
+
+    mapping(uint => Job) public jobs;
+    uint public jobCount;
+
+    event JobCreated(uint jobId, string description, uint budget, uint deadline);
+    event FreelancerBid(uint jobId, address freelancer, uint bidAmount);
+    event JobAssigned(uint jobId, address freelancer);
+    event JobCompleted(uint jobId, bool clientApproved);
+
+    function createJob(string memory description, uint budget, uint deadline) public {
+        // Increment job count and create a new job
+        jobCount++;
+        jobs[jobCount] = Job({
+            client: msg.sender,
+            description: description,
+            budget: budget,
+            deadline: deadline,
+            selectedFreelancer: address(0),
+            jobCompleted: false,
+            clientApproved: false
+        });
+        
+        emit JobCreated(jobCount, description, budget, deadline);
+    }
+
+    function placeBid(uint jobId, uint bidAmount) public {
+        require(jobId <= jobCount, "Invalid job ID");
+        Job storage job = jobs[jobId];
+        require(job.selectedFreelancer == address(0), "Job already assigned");
+
+        // Logic to handle bidding and select a freelancer
+        // This would include validating the bid and selecting the freelancer based on criteria
+
+        emit FreelancerBid(jobId, msg.sender, bidAmount);
+    }
+
+    function assignJob(uint jobId, address freelancer) public {
+        // Logic to assign the job to the selected freelancer
+
+        emit JobAssigned(jobId, freelancer);
+    }
+
+    function completeJob(uint jobId, bool clientApproved) public {
+        // Logic to handle job completion and payment release
+
+        emit JobCompleted(jobId, clientApproved);
+    }
+}
